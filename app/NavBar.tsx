5beb263b7c555c5e31022d6df6c9bf2f3656eec8
Trend20 <enockomondi305@gmail.com>
2023-10-20 17:55:04 +0300

    built the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,29 @@
 import Link from 'next/link';
-import React from 'react'
+import { SiPivotaltracker} from 'react-icons/si'
 
 const NavBar = () => {
+
+  // define the nav_links array
+  const navLinks =[
+    {
+      label:'Dashboard',
+      url:'/'
+    },
+    {
+      label:'Issues',
+      url:'/issues'
+    }
+  ]
+
   return (
-    <nav>
-      <Link href='/'>IssuePal</Link>
-      <ul>
-        <li><Link href='/'>Dashboard</Link></li>
-        <li><Link href='/issues'>Issues</Link></li>
+    <nav className='flex space-x-6 mb-5 px-5 h-14 border-b items-center'>
+      <Link href='/'><SiPivotaltracker size={30}/></Link>
+      <ul className='flex space-x-6'>
+        {
+          navLinks.map((link) =>(
+            <Link key={link.url} href={link.url} className='text-zinc-500 hover:text-zinc-800 transition-colors'>{link.label}</Link>
+          ))
+        }
       </ul>
     </nav>
   )
